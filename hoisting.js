3a39ba523cbb2183d02849e01622f04cb1e80f27
Keegan Rankin <kms.rankin@gmail.com>
2018-12-23 01:34:11 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -13,15 +13,17 @@ function crazy() {
   var thisIsCrazy = () => {
   console.log("hey!!!")
   }
+
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  function sayMy() {
-    var name = "Kristin";
-    console.log(name);
+  sayMy();
 
+  function sayMy() {
+    console.log(name);
+    var name = "Kristin";
   }
 }
